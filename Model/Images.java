3c0f10d59577505804b8d006b58bd1e76bc175fa
Bogdan <bogsacha@gmail.com>
2021-04-11 22:46:46 +0200

    Modification on Images Class

--- Model/Images.java
+++ Model/Images.java
@@ -2,22 +2,44 @@ package Model;
 
 import javax.swing.*;
 import java.io.File;
+import java.util.Objects;
 
 public class Images {
-    public ImageIcon[] listeImages;
-    private static String CHEMIN = "/Images";
+    // CONSTANTS
+    private final static String CHEMIN = "/Images";
+    private final static File FOLDER = new File(CHEMIN);
+    private final static int  NUMBER_OF_DISPONIBLE_IMAGES = Objects.requireNonNull(FOLDER.list()).length;
+
+    private ImageIcon[] listeImages;
+    private boolean[] listImagesReturned;
+
 
     public Images(int nbImage) {
         this.listeImages = new ImageIcon[nbImage];
         try {
-            File folder = new File(CHEMIN);
             int counter = 0;
-            for (final File fileEntry : folder.listFiles()) {
+            for (final File fileEntry : Objects.requireNonNull(FOLDER.listFiles())) {
                 listeImages[counter] = new ImageIcon(CHEMIN + '/' + fileEntry.getName());
                 counter++;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public ImageIcon[] getListeImages() {
+        return listeImages;
+    }
+
+    public void setListeImages(ImageIcon[] listeImages) {
+        this.listeImages = listeImages;
+    }
+
+    public boolean[] getListImagesReturned() {
+        return listImagesReturned;
+    }
+
+    public void setListImagesReturned(boolean[] listImagesReturned) {
+        this.listImagesReturned = listImagesReturned;
     }
 }
