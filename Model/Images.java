02ba272252c8faa3836770d395ffcb37c49fc2fc
Bogdan <bogsacha@gmail.com>
2021-04-12 00:56:25 +0200

    finish Images.java

--- Model/Images.java
+++ Model/Images.java
@@ -5,8 +5,9 @@ import java.io.File;
 import java.util.Random;
 
 public class Images {
+
     // CONSTANTS
-    private final static String CHEMIN = "/Images";
+    private final static String CHEMIN = "projet-swing-s2/Model/Images";
     private final static File FOLDER = new File(CHEMIN);
     private final static File[] IMAGE_FILES = FOLDER.listFiles();
     private final static int NUMBER_OF_DISPONIBLE_IMAGES = IMAGE_FILES.length;
@@ -18,10 +19,12 @@ public class Images {
     /**
      * Create a new intance of {@link Images} with {@nbImage} random picture affected to {@link Images#arrayImages} from {@link Images#FOLDER} using the Durstenfeld shuffle algorithm,
      * Throw an error if {@nbImage} is superior to {@link Images#NUMBER_OF_DISPONIBLE_IMAGES}
+     *
      * @param nbImage
      */
     public Images(int nbImage) {
-        if (NUMBER_OF_DISPONIBLE_IMAGES < nbImage){
+
+        if (NUMBER_OF_DISPONIBLE_IMAGES < nbImage) {
             System.out.println("Not enough images to hold the grid");
         }
 
@@ -32,22 +35,28 @@ public class Images {
          */
         int indexArrayImages = 0;
 
-        for (int lastIndexOf_IMAGE_FILES = NUMBER_OF_DISPONIBLE_IMAGES - 1 ; lastIndexOf_IMAGE_FILES > NUMBER_OF_DISPONIBLE_IMAGES - 1 - nbImage; lastIndexOf_IMAGE_FILES--){
+        for (int lastIndex = NUMBER_OF_DISPONIBLE_IMAGES - 1; lastIndex > NUMBER_OF_DISPONIBLE_IMAGES - 1 - nbImage; lastIndex--) {
 
-            int randomIndexOf_IMAGE_FILES = RANDOM.nextInt(lastIndexOf_IMAGE_FILES);
+            int randomIndexOf_IMAGE_FILES = RANDOM.nextInt(lastIndex);
             File currentImageFile = IMAGE_FILES[randomIndexOf_IMAGE_FILES];
 
             this.arrayImages[indexArrayImages] = new ImageIcon(CHEMIN + '/' + currentImageFile.getName());
             indexArrayImages++;
 
-            IMAGE_FILES[randomIndexOf_IMAGE_FILES] = IMAGE_FILES[lastIndexOf_IMAGE_FILES];
-            IMAGE_FILES[lastIndexOf_IMAGE_FILES] = currentImageFile;
+            IMAGE_FILES[randomIndexOf_IMAGE_FILES] = IMAGE_FILES[lastIndex];
+            IMAGE_FILES[lastIndex] = currentImageFile;
         }
     }
 
     public ImageIcon[] getArrayImages() {
         return arrayImages;
     }
+
+//    public void showArrayImages() {
+//        for (ImageIcon image : this.arrayImages) {
+//            System.out.println(image);
+//        }
+//    }
 
     public void setArrayImages(ImageIcon[] arrayImages) {
         this.arrayImages = arrayImages;
